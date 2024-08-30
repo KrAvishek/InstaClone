9e37ec12a33bd2b8af272186162684966e78578f
Kumar Abhishek <21052431@kiit.ac.in>
2024-08-31 00:47:18 +0530

    URL change

--- backend/index.js
+++ backend/index.js
@@ -75,7 +75,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin:'process.env.URL',
+    origin:process.env.URL,
     credentials: true
 }
 app.use(cors(corsOptions));
